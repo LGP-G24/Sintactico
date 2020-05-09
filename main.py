15ae46c292fd9ed7a6aca3c9db0e46f05ae23d2a
Davsatu313 <dsfajardob@unal.edu.co>
2020-05-09 18:17:54 +0000

    Now the variable path is almost done, because the list type is missing

--- main.py
+++ main.py
@@ -20,14 +20,25 @@ system('clear') #Antes de todo limpiamos la consola para evitar outputs anterior
 expectedToken = [] #en esta variable vamos a guardar lo que posiblemente esperamos
 currentIdent = 0 #Con esta variable vamos a revisar la identacion
 
+def var_def():
+  type_var()
+  expectedToken.append('tk_igual')
+  literal()
 
 def type_var():
   expectedToken.append('tk_dos_puntos')
+  chocoType()
+  return
 
 def chocoType():
-  types = ['int','str','object','bool','int','id']#hay que tener en cuenta la lista
-  
+  types = ['int','str','object','bool','id']#hay que tener en cuenta la lista
+  expectedToken.append(types)
+  return
 
+def literal():
+  types = ['tk_entero','True','False','None','tk_cadena','id']
+  expectedToken.append(types)
+  return  
 
 for token in tokens:
   if(token[0] == 't'): #es una nueva linea entonces reinicie
@@ -36,10 +47,14 @@ for token in tokens:
     continue
   if(len(expectedToken) < 1): #si esta vacio es una nueva linea
     #empezamos con una variable
-    print(token[0])
     if(token[0] == 'id'):
-      type_var()
+      var_def()
   else: #Ya deberia tener algo para comparar
-    
-
-
+    # print(expectedToken)
+    currentExpected = expectedToken.pop(0) #cojemos el primer elemento de el camino esperado
+    if(type(currentExpected) == list ): #si es una lista miramos que este en las posibilidades
+      if(token[0] not in currentExpected):
+        print('Error se esperaba',currentExpected,'pero se obtuvo',token[0]) #error
+    else:
+      if(token[0] != currentExpected):
+        print('Error se esperaba', currentExpected,'pero se obtuvo',token[0]) #error
